1c53a9cac532b4884fc19cbbcdd9066876ec9861
Hofma Barragan <hofma.barragan@gmail.com>
2026-02-28 14:27:48 +0100

    Fix sitemap domain mismatch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hofmanbarragan.com',
+  site: 'https://www.hofmahomes.com',
   integrations: [sitemap()],
 });
